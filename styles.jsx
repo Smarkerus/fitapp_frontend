ac639b7875137e30be44cf65c74c760909bfd7d7
Marcin <marcin_krolak@op.pl>
2025-04-24 22:04:03 +0200

    feat: inicjalizacja pliku z globalnymi stylami

---
+++ styles.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const globalStyles = StyleSheet.create({
+  button: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    margin: 5,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+});
