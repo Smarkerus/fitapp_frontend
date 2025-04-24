a202f21269424189d6f8e23362ff3f156a23bccc
Iga <bucz1991@gmail.com>
2025-04-24 19:36:46 +0200

    feat: Dodanie kontekstu autoryzacji

---
+++ context/AuthContext.jsx
@@ -0,0 +1,89 @@
+import React, { createContext, useState, useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { Platform } from "react-native";
+
+export const AuthContext = createContext();
+const BACKEND_URL = "http://localhost:8000/";
+
+export const AuthProvider = ({ navigation, children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      let token;
+      if (Platform.OS === "web") {
+        token = await AsyncStorage.getItem("userToken");
+      } else {
+        token = await SecureStore.getItemAsync("userToken");
+      }
+      if (token) {
+        try {
+          let userData = await fetchUserData(token);
+          setUser({ token: token, ...userData });
+          console.log("Udało się wczytać token:", token, userData, user);
+        } catch (error) {
+          await logout();
+        }
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    };
+    loadUser();
+  }, []);
+
+  const fetchUserData = async (token) => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}users/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("Pobrano info o użytkowniku: ", response.data);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const login = async (username, password) => {
+    try {
+      const formData = new URLSearchParams();
+      formData.append("username", username);
+      formData.append("password", password);
+
+      const response = await axios.post(`${BACKEND_URL}users/login`, formData.toString(), {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+
+      const access_token = response.data.access_token;
+
+      if (Platform.OS === "web") {
+        await AsyncStorage.setItem("userToken", access_token);
+      } else {
+        await SecureStore.setItemAsync("userToken", access_token);
+      }
+
+      const userData = await fetchUserData(access_token);
+      setUser({ token: access_token, ...userData });
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const logout = async () => {
+    if (Platform.OS === "web") {
+      await AsyncStorage.removeItem("userToken");
+    } else {
+      await SecureStore.deleteItemAsync("userToken");
+    }
+    setUser(null);
+  };
+
+  return <AuthContext.Provider value={{ user, login, logout, loading }}>{children}</AuthContext.Provider>;
+};
