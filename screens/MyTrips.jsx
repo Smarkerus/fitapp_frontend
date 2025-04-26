d41ec0f2562a9228490539dfc4e6dfbc1f0f4be7
Iga <bucz1991@gmail.com>
2025-04-26 20:18:07 +0200

    feat: dodano pobieranie informacji o trasach uzytkownika przy przechodzeniu do ekranu tras

--- screens/MyTrips.jsx
+++ screens/MyTrips.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
 import { View, Text } from 'react-native';
 import { globalStyles } from '../styles';
+import React, { useEffect, useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 export default function MyTrips() {
+  const { fetchUserTrips } = useContext(AuthContext);
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    const getTrips = async () => {
+      const userTrips = await fetchUserTrips();
+      setTrips(userTrips);
+      console.log('Pobrano informacje o trasach: ', userTrips);
+    };
+    getTrips();
+  }, []);
+
   return (
     <View style={globalStyles.background}>
       <View style={globalStyles.container}>
