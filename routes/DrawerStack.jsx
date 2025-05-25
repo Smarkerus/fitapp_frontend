7f0ce95383ab7f1f9aa3c4e60710e6864c07ea3b
Iga <bucz1991@gmail.com>
2025-05-25 17:31:36 +0200

    feat: Dodanie ikonek paska bocznego

--- routes/DrawerStack.jsx
+++ routes/DrawerStack.jsx
@@ -1,5 +1,6 @@
 import { useContext, lazy } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../screens/Home';
 const MyTrips = lazy(() => import('../screens/MyTrips'));
 const MyActivity = lazy(() => import('../screens/MyActivity'));
@@ -8,6 +9,8 @@ const Reminder = lazy(() => import('../screens/Reminder'));
 import { AuthContext } from '../context/AuthContext';
 
 const Drawer = createDrawerNavigator();
+
+const NullComponent = () => null;
 
 const DrawerStack = () => {
   const { logout } = useContext(AuthContext);
@@ -26,14 +29,53 @@ const DrawerStack = () => {
         drawerPosition: 'right',
       }}
     >
-      <Drawer.Screen name="Ekran Główny" component={Home} />
-      <Drawer.Screen name="Moje trasy" component={MyTrips} />
-      <Drawer.Screen name="Nowa aktywność" component={MyActivity} />
-      <Drawer.Screen name="Moje konto" component={Account} />
-      <Drawer.Screen name="Przypominajka" component={Reminder} />
       <Drawer.Screen
-        name="Wyloguj"
+        name="Ekran Główny"
         component={Home}
+        options={{
+          drawerLabel: 'Ekran Główny',
+          drawerIcon: ({ color }) => <Icon name="home" size={24} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        name="Moje trasy"
+        component={MyTrips}
+        options={{
+          drawerLabel: 'Moje trasy',
+          drawerIcon: ({ color }) => <Icon name="map" size={24} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        name="Nowa aktywność"
+        component={MyActivity}
+        options={{
+          drawerLabel: 'Nowa aktywność',
+          drawerIcon: ({ color }) => <Icon name="plus" size={24} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        name="Moje konto"
+        component={Account}
+        options={{
+          drawerLabel: 'Moje konto',
+          drawerIcon: ({ color }) => <Icon name="account" size={24} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        name="Przypominajka"
+        component={Reminder}
+        options={{
+          drawerLabel: 'Przypominajka',
+          drawerIcon: ({ color }) => <Icon name="alarm" size={24} color={color} />,
+        }}
+      />
+      <Drawer.Screen
+        component={NullComponent}
+        name="Wyloguj"
+        options={{
+          drawerLabel: 'Wyloguj',
+          drawerIcon: ({ color }) => <Icon name="logout" size={24} color={color} />,
+        }}
         listeners={{
           drawerItemPress: e => {
             e.preventDefault();
